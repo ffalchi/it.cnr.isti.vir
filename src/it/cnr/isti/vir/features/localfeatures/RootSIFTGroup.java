9cdf3c48a7c8284aadeba11f3a0a818e1568e27a
Fabrizio Falchi <fabrizio.falchi@cnr.it>
2013-02-12 14:25:47 +0100

    License was added to all java files.

--- src/it/cnr/isti/vir/features/localfeatures/RootSIFTGroup.java
+++ src/it/cnr/isti/vir/features/localfeatures/RootSIFTGroup.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Fabrizio Falchi (NeMIS Lab., ISTI-CNR, Italy)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 package it.cnr.isti.vir.features.localfeatures;
 
 import it.cnr.isti.vir.features.IFeaturesCollector;
@@ -209,6 +233,22 @@ public class RootSIFTGroup extends AbstractLFGroup<RootSIFT>  {
 		
 		return matches;
 	}
+	
+	static final public LocalFeaturesMatches getLoweMatches(AbstractLFGroup<RootSIFT> sg1, AbstractLFGroup<RootSIFT> sg2, double dRatioThr, int maxFDistSq) {
+		LocalFeaturesMatches matches = new LocalFeaturesMatches();
+		if ( sg2.size() < 2 ) return null;
+		int nMatches = 0;
+		RootSIFT[] arr = sg1.lfArr;
+		for (int i=0; i<arr.length; i++ ) {
+			RootSIFT match = RootSIFTGroup.getLoweMatch(arr[i], sg2, dRatioThr, maxFDistSq );
+			if ( match != null)
+				matches.add( new LocalFeatureMatch( arr[i], match ) );
+		}
+		
+		return matches;
+	}
+	
+	
 	/*
 	static final public LocalFeaturesMatches getLoweMatches(AbstractLFGroup<RootSIFT> sg1, AbstractLFGroup<RootSIFT> sg2, double dRatioThr) {
 		final LocalFeaturesMatches matches = new LocalFeaturesMatches();
