b5a69e0880342998695c499ca62f92ef4f7e4dd1
Fabrizio <Fabrizio@ligeti>
2014-01-21 16:18:17 +0100

    Bug removed.

--- src/it/cnr/isti/vir/util/MatrixMath.java
+++ src/it/cnr/isti/vir/util/MatrixMath.java
@@ -16,7 +16,7 @@ public class MatrixMath {
 	public static double[] times(double[][] matrix, double[] vector) {
 		double[] res = new double[matrix.length];
 		for(int i1=0; i1<matrix.length; i1++) {
-			for(int i2=0; i2<matrix.length; i2++) {
+			for(int i2=0; i2<vector.length; i2++) {
 				res[i1] += matrix[i1][i2] * vector[i2];
 			}
 		}
