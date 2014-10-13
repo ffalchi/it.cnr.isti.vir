009e247a2526a66a77ab3fc2401e756bf2af81b7
Fabrizio <Fabrizio@ligeti>
2014-10-13 09:50:47 +0200

    FloatsLF were added

---
+++ src/it/cnr/isti/vir/features/localfeatures/FloatsLF.java
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Fabrizio Falchi (NeMIS Lab., ISTI-CNR, Italy)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package it.cnr.isti.vir.features.localfeatures;
+
+import it.cnr.isti.vir.distance.L1;
+import it.cnr.isti.vir.distance.L2;
+import it.cnr.isti.vir.features.Floats;
+import it.cnr.isti.vir.features.IByteValues;
+import it.cnr.isti.vir.features.IFloatValues;
+import it.cnr.isti.vir.util.Mean;
+import it.cnr.isti.vir.util.bytes.FloatByteArrayUtil;
+
+import java.io.DataInput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class FloatsLF extends ALocalFeature<FloatsLFGroup> implements IFloatValues{
+	
+
+	final float[] values; 
+	
+	@Override
+	public final int getLength() {
+		return values.length;
+	}
+	
+	public final double getMmaxSQRDistValue() {
+		return 2.0;
+	}
+
+	@Override
+    public float[] getValues() {
+        return values;
+    }
+	
+	public FloatsLF(DataInput str ) throws IOException {
+            super(str);	
+		int dim = str.readByte();
+                // LEGACY
+                if ( dim < 0 ) dim &= 0xff;
+		values = new float[dim];
+		for ( int i=0; i<values.length; i++ ) {
+			values[i] = str.readFloat();
+		}
+	}
+	
+	public FloatsLF(ByteBuffer src ) throws IOException {
+                super(src);	
+		int dim = src.get();
+                // LEGACY
+                if ( dim < 0 ) dim &= 0xff;
+		values = new float[dim];
+		for ( int i=0; i<values.length; i++ ) {
+			values[i] = src.getFloat();
+		}
+	}
+        
+        public FloatsLF(KeyPoint kp, float[] values) {
+		this(kp, values, null);
+	}
+	
+	public FloatsLF(KeyPoint kp, float[] values, FloatsLFGroup group) {
+		this.kp = kp;
+		this.values = values;
+		this.linkedGroup = group;
+	}
+	
+	public FloatsLF(float[] values) {
+		super((KeyPoint) null, null);
+		this.values = values;
+	}
+
+	public int getDataByteSize() {
+		return values.length*4 +1;
+	}
+	
+	public int putDescriptor(byte[] bArr, int bArrI) {
+		bArr[bArrI++]  = (byte) values.length;
+		return FloatByteArrayUtil.convToBytes(values, bArr, bArrI);
+	}
+
+	@Override
+	public int compareTo(ALocalFeature<FloatsLFGroup> given) {
+		if ( this == given ) return 0;
+		if ( this.kp != given.kp ) {
+			if ( kp == null ) return -1;
+			if ( given.kp == null ) return 1;
+			int tComp = this.kp.compareTo( given.kp);	
+			if ( tComp != 0 ) return tComp;
+		}
+		for ( int i=0; i<values.length; i++ ) {
+			int tComp = Float.compare(values[i], ((FloatsLF)given).values[i]);
+			if ( tComp != 0 ) return tComp;
+		}
+		return 0;
+	}
+
+	@Override
+	public Class<FloatsLFGroup> getGroupClass() {
+		return FloatsLFGroup.class;
+	}
+	
+	public static final double getL1Distance(FloatsLF s1, FloatsLF s2) {
+		return L1.get(s1.values, s2.values);
+	}
+	
+
+	public static final double getL1Distance(FloatsLF s1, FloatsLF s2, double maxDist ) {
+		return L1.get(s1.values, s2.values, maxDist);
+	}
+	
+	public static final double getDistance_Norm(FloatsLF s1, FloatsLF s2 ) {
+		return Math.sqrt(getL1Distance(s1,s2)/s1.getMmaxSQRDistValue());
+	}
+
+	public static FloatsLF getMean(Collection<FloatsLF> coll) {
+		if ( coll.size() == 0 ) return null;
+		float[][] values = new float[coll.size()][];
+		int i=0;
+		for ( Iterator<FloatsLF> it = coll.iterator(); it.hasNext(); ) {
+			values[i++] = it.next().values;
+		}
+				
+		return new FloatsLF(Mean.getMean(values));		
+	}
+
+	
+	
+}
---
+++ src/it/cnr/isti/vir/features/localfeatures/FloatsLFGroup.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Fabrizio Falchi (NeMIS Lab., ISTI-CNR, Italy)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package it.cnr.isti.vir.features.localfeatures;
+
+import it.cnr.isti.vir.features.AbstractFeaturesCollector;
+import it.cnr.isti.vir.similarity.LocalFeatureMatch;
+import it.cnr.isti.vir.similarity.LocalFeaturesMatches;
+import it.cnr.isti.vir.util.Mean;
+
+import java.io.DataInput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class FloatsLFGroup extends ALocalFeaturesGroup<FloatsLF> {
+
+	public static final byte version = 1;
+	
+	public final byte getSerVersion() {
+		return version;
+	}
+	
+	public FloatsLFGroup(FloatsLF[] arr) {
+		super(arr);
+	}
+	
+	public FloatsLFGroup(FloatsLF[] arr, AbstractFeaturesCollector fc) {
+		super(arr, fc);
+	}
+
+	public FloatsLFGroup(AbstractFeaturesCollector fc) {
+		super(fc);
+	}
+
+	public FloatsLFGroup(DataInput in) throws Exception {
+		this(in, null);
+	}
+
+	
+	public FloatsLFGroup(ByteBuffer in) throws IOException {
+		byte version = in.get();
+		
+		int nBytes = in.getInt();
+		int nLFs = in.getInt();
+		lfArr = new FloatsLF[nLFs];
+		for ( int i = 0; i < nLFs; i++ ) {
+			lfArr[i] = new FloatsLF(in);
+			lfArr[i].setLinkedGroup(this);
+		}		
+		
+	}
+
+	public FloatsLFGroup(DataInput in, AbstractFeaturesCollector fc) throws Exception {
+		super(fc);
+		byte version = in.readByte();
+
+
+		int nBytes = in.readInt();
+		byte[] bytes = new byte[nBytes];
+		in.readFully(bytes);
+		ByteBuffer bBuffer = ByteBuffer.wrap(bytes);
+		lfArr = new FloatsLF[bBuffer.getInt()];
+		for (int i = 0; i < lfArr.length; i++) {
+			this.lfArr[i] = new FloatsLF(bBuffer);
+			lfArr[i].setLinkedGroup(this);
+		}
+
+	}
+
+	@Override
+	public Class getLocalFeatureClass() {
+		return FloatsLF.class;
+	}
+
+	@Override
+	public ALocalFeaturesGroup create(FloatsLF[] arr, AbstractFeaturesCollector fc) {
+		return new FloatsLFGroup( arr, fc);
+	}
+	
+	public static FloatsLF getMean(Collection<FloatsLF> coll) {
+		if ( coll.size() == 0 ) return null;
+		float[][] v = new float[coll.size()][];
+		int i=0;
+		for ( Iterator<FloatsLF> it = coll.iterator(); it.hasNext(); ) {
+			v[i++] = it.next().values;
+		}
+				
+		return new FloatsLF(Mean.getMean(v));		
+	}
+}
