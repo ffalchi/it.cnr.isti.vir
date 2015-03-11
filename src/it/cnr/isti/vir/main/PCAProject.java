275f457313a68bce35d8ac694a50efe94c13a61f
Fabrizio <Fabrizio@ligeti>
2015-03-11 16:27:20 +0100

    Multithread was added.

--- src/it/cnr/isti/vir/main/PCAProject.java
+++ src/it/cnr/isti/vir/main/PCAProject.java
@@ -9,13 +9,17 @@ import it.cnr.isti.vir.features.localfeatures.ALocalFeature;
 import it.cnr.isti.vir.features.localfeatures.ALocalFeaturesGroup;
 import it.cnr.isti.vir.features.localfeatures.FloatsLF;
 import it.cnr.isti.vir.features.localfeatures.FloatsLFGroup;
+import it.cnr.isti.vir.file.ArchiveException;
 import it.cnr.isti.vir.file.FeaturesCollectorsArchive;
 import it.cnr.isti.vir.global.Log;
+import it.cnr.isti.vir.global.ParallelOptions;
 import it.cnr.isti.vir.pca.PrincipalComponents;
 import it.cnr.isti.vir.util.PropertiesUtils;
 import it.cnr.isti.vir.util.TimeManager;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Properties;
 
 public class PCAProject {
@@ -28,7 +32,7 @@ public class PCAProject {
 		System.out.println("- PCAProject.outArchive=<archive file name>");
 		System.out.println("- PCA.PC=<principal components file>");
 		System.out.println("- [PCAProject.dim]");
-		System.out.println("- PCA.FeatureClass=<file name for PCA output>");
+		System.out.println("- PCA.FeatureClass=<feature class to be projects>");
 		System.exit(0);
 	}
 	
@@ -41,6 +45,104 @@ public class PCAProject {
 		}
 		
 	}
+	
+	static  class Project implements Runnable {
+		private final Iterator<AbstractFeaturesCollector> it;
+		private final FeaturesCollectorsArchive out;
+		private final PrincipalComponents pc;
+		private final Class c;
+		private final TimeManager tm;
+		
+		public Project(
+				Iterator<AbstractFeaturesCollector> it,
+				FeaturesCollectorsArchive out,
+				PrincipalComponents pc,
+				Class c,
+				TimeManager tm ) {
+			this.it = it;
+			this.out = out;
+			this.c = c;
+			this.tm = tm;
+			this.pc = pc;
+		}
+
+		@Override
+		public void run() {
+			AbstractFeaturesCollector fc = null;
+			while (true) {
+				synchronized ( it ) {
+					if ( it.hasNext() ) fc = it.next();
+					else break;
+				}
+				Log.info_verbose_progress(tm, out.size());
+				
+				AbstractFeature currf = fc.getFeature(c);
+				Floats projected = new Floats(pc.project_float( (IArrayValues) currf));
+				
+				AbstractFeaturesCollector newFC = fc.createWithSameInfo(projected);
+
+				try {
+					out.add(newFC);
+				} catch (ArchiveException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+	
+	static  class Project_LF implements Runnable {
+		private final Iterator<AbstractFeaturesCollector> it;
+		private final FeaturesCollectorsArchive out;
+		private final PrincipalComponents pc;
+		private final Class<ALocalFeaturesGroup> lfGroupClass;
+		private final TimeManager tm;
+		
+		public Project_LF(
+				Iterator<AbstractFeaturesCollector> it,
+				FeaturesCollectorsArchive out,
+				PrincipalComponents pc,
+				Class<ALocalFeaturesGroup> lfGroupClass,
+				TimeManager tm ) {
+			this.it = it;
+			this.out = out;
+			this.lfGroupClass = lfGroupClass;
+			this.tm = tm;
+			this.pc = pc;
+		}
+
+		@Override
+		public void run() {
+			AbstractFeaturesCollector fc = null;
+			while (true) {
+				synchronized ( it ) {
+					if ( it.hasNext() ) fc = it.next();
+					else break;
+				}
+				Log.info_verbose_progress(tm, out.size());
+				
+				ALocalFeaturesGroup currGroup = fc.getFeature(lfGroupClass);
+				ALocalFeature[] currLFs = currGroup.lfArr;
+				FloatsLF[] projected = new FloatsLF[currGroup.size()];
+				for ( int i=0; i<currLFs.length; i++ ) {
+					projected[i] = new FloatsLF(pc.project_float( (IArrayValues) currLFs[i])); 
+				}
+				FloatsLFGroup projectedGroup = new FloatsLFGroup(projected);
+				AbstractFeaturesCollector newFC = fc.createWithSameInfo(projectedGroup);
+				projectedGroup.setLinkedFC(newFC);
+				
+				try {
+					out.add(newFC);
+				} catch (ArchiveException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+	
 	
 	public static void launch(Properties prop) throws Exception {
 		
@@ -63,39 +165,79 @@ public class PCAProject {
 		
 		int count=0;
 		TimeManager tm = new TimeManager();
+		tm.setTotNEle(in.size());
 		if ( ALocalFeature.class.isAssignableFrom(c)  ) {
 			Class<ALocalFeaturesGroup> lfGroupClass = ALocalFeaturesGroup.getGroupClass( c );
 
-			for ( AbstractFeaturesCollector fc : in ) {
-				count++;
-				Log.info_verbose_progress(tm, count, in.size());
-				
-				ALocalFeaturesGroup currGroup = fc.getFeature(lfGroupClass);
-				ALocalFeature[] currLFs = currGroup.lfArr;
-				FloatsLF[] projected = new FloatsLF[currGroup.size()];
-				for ( int i=0; i<currLFs.length; i++ ) {
-					projected[i] = new FloatsLF(pc.project_float( (IArrayValues) currLFs[i])); 
-				}
-				FloatsLFGroup projectedGroup = new FloatsLFGroup(projected);
-				AbstractFeaturesCollector newFC = fc.createWithSameInfo(projectedGroup);
-				projectedGroup.setLinkedFC(newFC);
-				
-				out.add(newFC);
-			}
-		} else {
+			int nThread = ParallelOptions.reserveNFreeProcessors() +1 ;
 			
-			for ( AbstractFeaturesCollector fc : in ) {
-				count++;
-				if ( tm.hasToOutput() )
-					Log.info_verbose(tm.getProgressString(count, in.size()));
-				
-				AbstractFeature currf = fc.getFeature(c);
-				Floats projected = new Floats(pc.project_float( (IArrayValues) currf));
-				
-				AbstractFeaturesCollector newFC = fc.createWithSameInfo(projected);
-
-				out.add(newFC);
+			Thread[] thread = new Thread[nThread];
+			Iterator<AbstractFeaturesCollector> it = in.iterator();
+			for ( int ti=0; ti<nThread; ti++ ) {
+				thread[ti] = new Thread(new Project_LF(it, out, pc, c, tm ));
+				thread[ti].start();
 			}
+		        
+	        for ( int ti=0; ti<thread.length && thread[ti] != null; ti++ ) {
+	        	try {
+					thread[ti].join();
+					thread[ti] = null;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+	        }
+		        
+	        ParallelOptions.free(nThread-1);
+			
+//			for ( AbstractFeaturesCollector fc : in ) {
+//				count++;
+//				Log.info_verbose_progress(tm, count, in.size());
+//				
+//				ALocalFeaturesGroup currGroup = fc.getFeature(lfGroupClass);
+//				ALocalFeature[] currLFs = currGroup.lfArr;
+//				FloatsLF[] projected = new FloatsLF[currGroup.size()];
+//				for ( int i=0; i<currLFs.length; i++ ) {
+//					projected[i] = new FloatsLF(pc.project_float( (IArrayValues) currLFs[i])); 
+//				}
+//				FloatsLFGroup projectedGroup = new FloatsLFGroup(projected);
+//				AbstractFeaturesCollector newFC = fc.createWithSameInfo(projectedGroup);
+//				projectedGroup.setLinkedFC(newFC);
+//				
+//				out.add(newFC);
+//			}
+		} else {
+			int nThread = ParallelOptions.reserveNFreeProcessors() +1 ;
+			
+			Thread[] thread = new Thread[nThread];
+			Iterator<AbstractFeaturesCollector> it = in.iterator();
+			for ( int ti=0; ti<nThread; ti++ ) {
+				thread[ti] = new Thread(new Project(it, out, pc, c, tm ));
+				thread[ti].start();
+			}
+		        
+	        for ( int ti=0; ti<thread.length && thread[ti] != null; ti++ ) {
+	        	try {
+					thread[ti].join();
+					thread[ti] = null;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+	        }
+		        
+	        ParallelOptions.free(nThread-1);
+			
+//			for ( AbstractFeaturesCollector fc : in ) {
+//				count++;
+//				if ( tm.hasToOutput() )
+//					Log.info_verbose(tm.getProgressString(count, in.size()));
+//				
+//				AbstractFeature currf = fc.getFeature(c);
+//				Floats projected = new Floats(pc.project_float( (IArrayValues) currf));
+//				
+//				AbstractFeaturesCollector newFC = fc.createWithSameInfo(projected);
+//
+//				out.add(newFC);
+//			}
 		}
 		
 		in.close();
